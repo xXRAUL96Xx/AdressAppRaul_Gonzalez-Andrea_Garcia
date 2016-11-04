af604fef2da4b2abeb2d9edcc4426e687f7ad009
Raul <xxraul96xx@outlook.es>
2016-11-04 12:17:07 +0100

    Tutorial 5

--- AddressApp/src/ch/makery/address/model/Person.java
+++ AddressApp/src/ch/makery/address/model/Person.java
@@ -2,6 +2,9 @@ package ch.makery.address.model;
 
 import java.time.LocalDate;
 
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import ch.makery.address.util.LocalDateAdapter;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleIntegerProperty;
@@ -106,7 +109,7 @@ public class Person {
     public StringProperty cityProperty() {
         return city;
     }
-
+    @XmlJavaTypeAdapter(LocalDateAdapter.class)
     public LocalDate getBirthday() {
         return birthday.get();
     }
